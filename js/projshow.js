1d11ff2a35703d549c3e10f287d8cae08228f434
bh4c9 <huangbq.01@gmail.com>
2017-02-26 19:42:32 +0000

    QSPF initial commit

---
+++ js/projshow.js
@@ -0,0 +1,13 @@
+jQuery( document ).ready(function( $ ) {
+    
+    var slider = $('.bxslider').bxSlider({
+        pagerCustom: "#bx-pager",
+        mode:"fade",
+        nextSelector: '#slider-next',
+        prevSelector: '#slider-prev',
+        nextText: 'Next',
+        prevText: 'Previous'
+    });
+    
+    
+});
